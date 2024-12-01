9d3eef950f5b1e60e23a96250c29e484a34723ac
原田明斗 <harada.akito@haradaakiratonoMacBook-Pro.local>
2024-12-01 17:16:55 +0900

    do exercise9

--- exercises/09_strings/strings1.rs
+++ exercises/09_strings/strings1.rs
@@ -1,9 +1,10 @@
 // TODO: Fix the compiler error without changing the function signature.
-fn current_favorite_color() -> String {
-    "blue"
+fn current_favorite_color(s:&str) -> &str {
+    s
 }
 
 fn main() {
-    let answer = current_favorite_color();
+    let favorite_color = "blue";
+    let answer = current_favorite_color(favorite_color);
     println!("My current favorite color is {answer}");
 }
--- exercises/09_strings/strings4.rs
+++ exercises/09_strings/strings4.rs
@@ -1,5 +1,14 @@
 // Calls of this function should be replaced with calls of `string_slice` or `string`.
-fn placeholder() {}
+enum Input {
+    String(String),
+    Str(&'static str),
+}
+fn placeholder(input: Input) {
+    match input {
+        Input::String(s) => string(s),
+        Input::Str(s) => string_slice(s),
+    }
+}
 
 fn string_slice(arg: &str) {
     println!("{arg}");
@@ -13,25 +22,25 @@ fn string(arg: String) {
 // Your task is to replace `placeholder(…)` with either `string_slice(…)`
 // or `string(…)` depending on what you think each value is.
 fn main() {
-    placeholder("blue");
+    placeholder(Input::Str("blue"));
 
-    placeholder("red".to_string());
+    placeholder(Input::String("red".to_string()));
 
-    placeholder(String::from("hi"));
+    placeholder(Input::String(String::from("hi")));
 
-    placeholder("rust is fun!".to_owned());
+    placeholder(Input::String("rust is fun!".to_owned()));
 
-    placeholder("nice weather".into());
+    placeholder(Input::Str("nice weather".into()));
 
-    placeholder(format!("Interpolation {}", "Station"));
+    placeholder(Input::String(format!("Interpolation {}", "Station")));
 
     // WARNING: This is byte indexing, not character indexing.
     // Character indexing can be done using `s.chars().nth(INDEX)`.
-    placeholder(&String::from("abc")[0..1]);
+    placeholder(Input::Str(&"abc"[0..1]));
 
-    placeholder("  hello there ".trim());
+    placeholder(Input::Str("  hello there ".trim()));
 
-    placeholder("Happy Monday!".replace("Mon", "Tues"));
+    placeholder(Input::String("Happy Monday!".replace("Mon", "Tues")));
 
-    placeholder("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    placeholder(Input::String("mY sHiFt KeY iS sTiCkY".to_lowercase()));
 }
